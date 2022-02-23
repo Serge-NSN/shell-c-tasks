9c2b0724354def9b67551a32367540851cc4ce9e
Ntunyu Serge <81696072+Serge-NSN@users.noreply.github.com>
2022-02-23 18:56:40 +0100

    Bash file

--- exercise1.sh
+++ exercise1.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 gcc -o exercise1 exercise1.c
 
-FILE=exercise1.c
-if [ -f "$FILE" ]; then
-	./exercise1
+FILE=touchfd.txt
+if [[ -f"$FILE" ]]; 
+then
+	if [[ "$FILE" == *".c" ]]; 
+	then
+		./exercise1
+	else
+		echo "File is not a C file"
+	fi
 else
 	echo "File does not exist."
+
 fi
 
