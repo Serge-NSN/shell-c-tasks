7ca10da53fcc8acbf0999c1a4f687eb189e34f68
Ntunyu Serge <81696072+Serge-NSN@users.noreply.github.com>
2022-02-26 21:27:03 +0100

    Exercise 2 - Incomplete.
    Creating two functions where one counts the number of characters in file and the other performs the tasks of copying the content of a given file to 2 other files by splitting its main content. This is incoplete tho.

---
+++ exercise2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+//function to count the characters in text file
+
+int count_characters(file_path[]){
+
+	char file_path[]="";
+	int ch=0;
+	int fd;
+	int fd=open("$file_path", O_RDONLY);
+	
+	if (fd<0){
+		printf("Encountered error opening file\n");
+		}
+	else
+	{
+		while(ch = fgetc(fd) != EOF)
+		{
+			ch++;
+		}
+	close(fd);
+	
+	return ch;		
+}
+
+
+void copy_text(main_file[]){ //The main_file here is a file path to the original file
+	char main_file[]="";
+	int fd=open("$main_file", O_RDONLY);
+}
